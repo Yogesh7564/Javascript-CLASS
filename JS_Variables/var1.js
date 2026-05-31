12cc343bdd6989e7f4b6c05e682c96a8a2a96b24
yogesh <yogeshhk098@gmail.com>
2026-05-31 15:05:45 +0530

    new files added

---
+++ JS_Variables/var1.js
@@ -0,0 +1,10 @@
+
+let age=null
+console.log(age)
+age=40
+console.log(age)
+
+/*
+let b=null
+console.log(b)
+*/
---
+++ JS_Variables/variables.js
@@ -0,0 +1,6 @@
+var name="virat"
+let age=18
+const city="delhi"
+console.log(name)
+console.log(age)
+console.log(city)
